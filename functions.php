02c403a8586fa797ae5c3010e977431548ee567a
Luiz "Bills <luizpbills@gmail.com>
2016-07-10 22:50:05 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,10 +1,2 @@
 <?php
-/*
-	Disable the default theme stylesheet (style.css)
-	- When not used, prevents 1 file request.
-*/
-add_action( 'wp_enqueue_scripts', 'gpchild_remove_default_style', 20 );
-function gpchild_remove_default_style() {
-    wp_dequeue_style( 'generate-child' );
-    wp_deregister_style( 'generate-child' );
-}
+
