c9d252c243d5be7b6738a669174a2b96bcbb7200
Luiz "Bills <luizpbills@gmail.com>
2016-08-15 18:24:37 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,2 +1,9 @@
 <?php
 
+require 'inc/gpchild-functions.php';
+require 'inc/gpchild-template-functions.php';
+require 'inc/gpchild-template-hooks.php';
+
+require 'inc/gpchild-woocommerce-functions.php';
+require 'inc/gpchild-woocommerce-template-functions.php';
+require 'inc/gpchild-woocommerce-template-hooks.php';
