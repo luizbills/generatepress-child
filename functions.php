2d5a546556cfb115a0cdc575f6aa316cbd054415
Luiz "Bills <luizpbills@gmail.com>
2016-08-16 11:17:39 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,14 @@
 <?php
 
+$active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins' ) );
+
 require 'inc/gpchild-functions.php';
 require 'inc/gpchild-template-functions.php';
 require 'inc/gpchild-template-hooks.php';
 
-require 'inc/gpchild-woocommerce-functions.php';
-require 'inc/gpchild-woocommerce-template-functions.php';
-require 'inc/gpchild-woocommerce-template-hooks.php';
+if ( in_array( 'woocommerce/woocommerce.php', $active_plugins ) ) {
+  require 'inc/gpchild-woocommerce-functions.php';
+  require 'inc/gpchild-woocommerce-template-functions.php';
+  require 'inc/gpchild-woocommerce-template-hooks.php';
+}
+
